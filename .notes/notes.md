09da9b1bc9c00b4bcd13c22f0f4873a305304167
Nathan Landis <nathanlandis@gmail.com>
2024-02-24 00:18:26 -0600

    Generate web app API wrapper

---
+++ .notes/notes.md
@@ -0,0 +1,5 @@
+# Some varied project notes
+
+- be able to post a file to a website -> s3
+- be able to list/download files on a website <- s3
+- no/lite auth
--- code/typescript/nebu-web/src/components/buckets/BucketList.tsx
+++ code/typescript/nebu-web/src/components/buckets/BucketList.tsx
@@ -1,9 +1,20 @@
+import { useGetApiBuckets } from "../../api/buckets/buckets";
 import Container from "../container/Container";
 
 const BucketList = () => {
+    const bux = useGetApiBuckets();
+    if (bux.isError) {
+        console.error(bux.error);
+    }
+
     return (
         <Container>
-            Here are your buckets, look at them
+            Here are your buckets, look at them!
+            <ul>
+                {bux.data?.data.map(b => (
+                    <li key={b.id}>{b.id}: {b.uri}</li>
+                ))}
+            </ul>
         </Container>
     )
 }
